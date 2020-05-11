9a8ea9cd1ff6d6e16cf2acc5816abd11fd04e778
Marcos R F Rosada <ochild@gmail.com>
2020-05-11 16:05:19 +0100

    Dashboard -> Sign Out

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -5,6 +5,7 @@ interface AuthContextData {
   signed: boolean;
   user: object | null;
   signIn(): Promise<void>;
+  signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -17,8 +18,12 @@ export const AuthProvider: React.FC = ({ children }) => {
 
     setUser(response.user);
   }
+
+  function signOut() {
+    setUser(null);
+  }
   return (
-    <AuthContext.Provider value={{ signed: !!user, user, signIn }}>
+    <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
 
-const Dashboard: React.FC = () => <View />;
+import AuthContext from '../../contexts/auth';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+const Dashboard: React.FC = () => {
+  const { signOut } = useContext(AuthContext);
+
+  function handleSignOut() {
+    signOut();
+  }
+  return (
+    <View style={styles.container}>
+      <Button title="Sign Out" onPress={handleSignOut} />
+    </View>
+  );
+};
 
 export default Dashboard;
