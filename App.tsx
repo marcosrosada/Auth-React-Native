67113b70396b21e0830356aac91590e9d1b3ef36
Marcos R F Rosada <ochild@gmail.com>
2020-05-11 14:03:52 +0100

    Pages

--- App.tsx
+++ App.tsx
@@ -1,10 +1,17 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { View } from 'react-native';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const SignIn: React.FC = () => <Text>Hello Native</Text>;
+
+export default SignIn;
